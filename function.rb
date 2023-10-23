c68c8b6b96dcac348764ec894e1b97c4227ef69a
harsha-miryala <miryalaharshavardhan4@gmail.com>
2023-10-22 18:41:35 -0700

    Initial Config

--- function.rb
+++ function.rb
@@ -7,7 +7,75 @@ require 'pp'
 def main(event:, context:)
   # You shouldn't need to use context, but its fields are explained here:
   # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-  response(body: event, status: 200)
+  headers = event[HEADERS].keys
+  for header in headers
+    if header.casecmp?(CONTTYPE)
+      event[HEADERS][CONTTYPE] = event[HEADERS][header]
+    end
+    
+    if header.casecmp?(AUTH)
+      event[HEADERS][AUTH] = event[HEADERS][header]
+    end
+  end
+  
+  case event["httpMethod"]
+  when "GET"
+    begin
+      if event[PATH] == "/token"
+        return response(body: nil, status: 405)
+      end
+      
+      if event[PATH] != "/"
+        return response(body: nil, status:404)
+      end
+      
+      if event[HEADERS][AUTH].split(" ")[0] != "Bearer"
+        return response(body: nil, status: 403)
+      end
+      
+      token = event[HEADERS][AUTH].split(" ")[1]
+      payload = JWT.decode(token, "SECRET_KEY")
+      
+    rescue JWT::ImmatureSignature, JWT::ExpiredSignature => e
+      return response(body: nil, status: 401)
+      
+    rescue JWT::DecodeError => e
+      return response(body: nil, status: 403)
+      
+    rescue
+      return response(body: nil, status: 403)
+      
+    else
+      return response(body: payload[0]["data"], status: 200)
+    end
+    
+  when "POST"
+    if event[PATH] == "/"
+      return response(body: nil, status: 405)
+    end
+    
+    if event[HEADERS][CONTTYPE] != "application/json"
+      return response(body: nil, status: 415)
+    end
+    
+    begin
+      JSON.parse(event["body"])
+    rescue
+      return response(body: event, status: 422)
+    else
+      payload = {
+        data: JSON.parse(event["body"]),
+        exp: Time.now.to_i + 10,
+        nbf: Time.now.to_i
+      }
+      
+      token = JWT.encode payload, "SECRET_KEY", 'HS256'
+      return response(body: {"token" => token}, status: 201)
+    end
+  
+  else
+    return response(body: nil, status: 405)
+  end
 end
 
 def response(body: nil, status: 200)
